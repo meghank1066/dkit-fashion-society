987f64cff00e64f20c69feaa9f43c82df31cb571
meg <119862594+meghank1066@users.noreply.github.com>
2026-08-05 02:10:11 +0100

    vercel fix

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -478,6 +478,7 @@ export default function AdminDashboard() {
                     </td>
                   </tr>
                 )}
+                {/* save */}
               </tbody>
             </table>
           </div>
