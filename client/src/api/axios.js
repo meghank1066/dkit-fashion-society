c7a622ff995b314c36ada904c3286d16dc671ccb
meg <119862594+meghank1066@users.noreply.github.com>
2026-08-05 17:14:08 +0100

    api fix lets see

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-const API = axios.create({
-    // github backend deployment
-  baseURL: "https://dkit-fashion-backend.onrender.com",
+// const API = axios.create({
+//     // github backend deployment
+//   baseURL: "https://dkit-fashion-backend.onrender.com",
+// });
+
+const res = await API.post("/upload", formData, {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+    "Content-Type": "multipart/form-data"
+  }
 });
 
 export default API;
