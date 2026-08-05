624633090271207aefc1f6065308f7139d4da944
meg <119862594+meghank1066@users.noreply.github.com>
2026-08-05 02:15:55 +0100

    fixing login routes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -58,7 +58,7 @@ export default function Register(){
 
         try {
 
-            await API.post("/auth/register", {
+         await API.post("/api/auth/register", {
                 username,
                 email,
                 password
