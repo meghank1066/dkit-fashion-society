af92d7560b33d9337eb29b299f10d936e9a6b04d
meg <119862594+meghank1066@users.noreply.github.com>
2026-08-05 01:56:13 +0100

    axios live-change

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+  baseURL: "https://dkit-fashion-backend.onrender.com",
 });
-
 
 export default API;
