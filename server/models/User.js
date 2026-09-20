214adf91f7332f64f1502201ea019d65a10cc330
meg <119862594+meghank1066@users.noreply.github.com>
2026-09-20 18:46:56 +0100

    lowercase email fix issue

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true, lowercase: true, trim: true },
         password: { type: String, required: true },
         role: { type: String, default: "user" },
         profileImage: { type: String, default: "" },
